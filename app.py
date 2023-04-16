b7a885797b0eee0a7b3dde55089116316eef8ba5
Dania Irshad <daniairshad@live.com>
2023-04-16 14:58:35 -0500

    add profile pages

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, flash, redirect, session, g, request, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
-from forms import UserAddForm, LoginForm
+from forms import UserAddForm, LoginForm, UserEditForm
 from models import db, connect_db, User
 import requests
 
@@ -20,33 +20,11 @@ connect_db(app)
 
 
 ##############################################################################
-# Homepage and error pages
-
-@app.route('/')
-def homepage():
-    """Show homepage"""
-    user = g.user
-    if user:
-        res = requests.get("https://www.themealdb.com/api/json/v1/1/categories.php")
-        data = res.json()
-        categories = data.get('categories')
-        return render_template('home.html', user=user, categories=categories)
-    else:
-        return redirect('/login')
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """404 NOT FOUND page."""
-
-    return render_template('404.html'), 404
-
-
-##############################################################################
 # User signup/login/logout
 
 @app.before_request
 def add_user_to_g():
-    """If we're logged in, add curr user to Flask global."""
+    """If user is logged in, add curr user to Flask global."""
 
     if CURR_USER_KEY in session:
         g.user = User.query.get(session[CURR_USER_KEY])
@@ -94,12 +72,12 @@ def signup():
             db.session.commit()
 
         except IntegrityError:
-            flash("Username already taken", 'danger')
+            flash('Username already taken', 'danger')
             return render_template('signup.html', form=form)
 
         do_login(user)
 
-        return redirect("/")
+        return redirect('/')
 
     else:
         return render_template('signup.html', form=form)
@@ -117,10 +95,10 @@ def login():
 
         if user:
             do_login(user)
-            flash(f"Hello, {user.username}!", "success")
-            return redirect("/")
+            flash(f'Hello, {user.username}!', 'success')
+            return redirect('/')
 
-        flash("Invalid credentials.", 'danger')
+        flash('Invalid credentials.', 'danger')
 
     return render_template('login.html', form=form)
 
@@ -135,13 +113,36 @@ def logout():
 
 
 ##############################################################################
+# Homepage and error pages
+
+@app.route('/')
+def homepage():
+    """Display the logged in user homepage"""
+    user = g.user
+    if user:
+        res = requests.get('https://www.themealdb.com/api/json/v1/1/categories.php')
+        data = res.json()
+        categories = data.get('categories')
+        return render_template('home.html', user=user, categories=categories)
+    else:
+        return redirect('/login')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """404 NOT FOUND page."""
+
+    return render_template('404.html'), 404
+
+
+##############################################################################
 # Recipes
 
 @app.route('/category/<category_name>')
 def get_category(category_name):
+    """Display all recipes the selected category"""
     user = g.user
     if user:
-        res = requests.get(f"https://www.themealdb.com/api/json/v1/1/filter.php",
+        res = requests.get(f'https://www.themealdb.com/api/json/v1/1/filter.php',
                            params={'c': category_name})
         data = res.json()
         recipes = data.get('meals')
@@ -149,21 +150,13 @@ def get_category(category_name):
     else:
         return redirect('/login')
 
-@app.route('/recipe', methods=['POST'])
-def search():
-    user = g.user
-    if user:
-        search_value = request.form['s']
-        return redirect(url_for('get_recipe', recipe_name=search_value))
-    else:
-        return redirect('/login')
-
 
 @app.route('/recipe/<recipe_name>')
 def get_recipe(recipe_name):
+    """Display the selected recipe"""
     user = g.user
     if user:
-        res = requests.get(f"https://www.themealdb.com/api/json/v1/1/search.php",
+        res = requests.get(f'https://www.themealdb.com/api/json/v1/1/search.php',
                            params={'s': recipe_name})
         data = res.json()
         recipe = data.get('meals')[0]
@@ -184,8 +177,82 @@ def get_recipe(recipe_name):
 
 @app.route('/about')
 def about():
+    """Display the about page."""
     user = g.user
     if user:
         return render_template('about.html')
+    else:
+        return redirect('/login')
+    
+@app.route('/recipe', methods=['POST'])
+def search():
+    """Search a recipe"""
+    user = g.user
+    if user:
+        search_value = request.form['s']
+        return redirect(url_for('get_recipe', recipe_name=search_value))
+    else:
+        return redirect('/login')
+
+@app.route('/user/<int:user_id>')
+def user_profile(user_id):
+    """Display user profile."""
+    user = g.user
+    if user:
+        curr_user = User.query.get_or_404(user_id)
+        if curr_user == user:
+            return render_template('profile.html', user=user)
+        else:
+            return redirect('/')
+    else:
+        return redirect('/login')
+
+
+##############################################################################
+# Profile
+
+@app.route('/user/<int:user_id>/edit', methods=["GET", "POST"])
+def edit_profile(user_id):
+    """Edit user profile."""
+    user = g.user
+    if user:
+        curr_user = User.query.get_or_404(user_id)
+        if curr_user == user:
+            form = UserEditForm(obj=user)
+            if form.validate_on_submit():
+                if User.authenticate(user.username, form.password.data):
+                    try:
+                        user.username=form.username.data,
+                        user.email=form.email.data,
+                        user.image_url=form.image_url.data or User.image_url.default.arg
+                        db.session.commit()
+                    except IntegrityError:
+                        flash('Username already taken', 'danger')
+                        db.session.rollback()
+                        return render_template('edit.html', form=form, user=user)
+                    return redirect(f'/user/{user.id}')
+                else:
+                    flash('Wrong Password.', 'danger')
+                    return render_template('edit.html', form=form, user=user)
+            return render_template('edit.html', form=form, user=user)
+        else:
+            return redirect('/')
+    else:
+        return redirect('/login')
+    
+
+@app.route('/user/<int:user_id>/delete', methods=["GET", "POST"])
+def delete_user(user_id):
+    """Delete user"""
+    user = g.user
+    if user:
+        curr_user = User.query.get_or_404(user_id)
+        if curr_user == user:
+            do_logout()
+            db.session.delete(g.user)
+            db.session.commit()
+            return redirect('/signup')
+        else:
+            return redirect('/')
     else:
         return redirect('/login')
